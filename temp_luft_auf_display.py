c2f60408bad56ae9bf293dea774438e21a746588
maaaxxx23 <142886773+maaaxxx23@users.noreply.github.com>
2023-12-05 14:11:11 +0100

    v0.2 Kommentare ergänzt

--- temp_luft_auf_display.py
+++ temp_luft_auf_display.py
@@ -6,7 +6,41 @@ Funktionsbeschreibung:
 Das Programm zeigt die Temperatur und die Luftfeuchtigkeit auf einem Display an.
 Die Steuerung muss vorher über den Taster eingeschaltet werden.
 Die Steuerung lässt sich durch den Taster an und aus schalten.
-Version: 0.1; 21.11.2023
+Version: 0.2; 05.12.2023
+Hardware: ESP32 USB-C; AHT10 - Sensor; LCD - Display 240 x 280; 1x LED - rot; 1x LED - gruen; 1x 5.1KOhm Widerstand; 2x 330Ohm Widerstand
+
+Anschlüsse: Display:
+            GND: GND
+            VCC: 3.3V
+            SCL:  13
+            SDA:  11
+            RES:   5
+            DC:    4
+            CS:
+            BLK: 3.3V
+            
+            AHT10:
+            VCC: 3.3V
+            GND: GND
+            SCL:   9
+            SDA:   8
+            
+            Taster:
+            1. Pin: 3.3V
+            2. Pin: 7 / 5.1KOhm auf GND(Pulldown)
+            
+            LED rot:
+            GND
+            6 / 330 Ohm
+            
+            LED gruen:
+            GND
+            21 / 330 Ohm
+            
+            
+
+Bibliotheken: AHTx0: https://github.com/targetblank/micropython_ahtx0
+              st7789: https://github.com/russhughes/st7789_mpy
 """
 
 import uos
@@ -63,12 +97,13 @@ display.fill(st7789.BLACK)
 
 # Hauptprogramm
 while True:
+    
     temperatur = sensor.temperature
     luftfeuchtigkeit = sensor.relative_humidity
     
-    aktueller_taster_status = taster.value()
-    utime.sleep(0.2)
-    if aktueller_taster_status and not letzter_taster_status:
+    aktueller_taster_status = taster.value() # der aktuelle Taster Zustand wird in die aktueller_taster_status Variable geschrieben
+    utime.sleep(0.2) # Zeit zum entprellen des Tasters 
+    if aktueller_taster_status and not letzter_taster_status: # Es wird verglichen, ob der aktuelle_taster_status dem letzten_taster_status entspricht. Wenn nicht dann geht es weiter
         power_an = not power_an
     
         if temperatur >= 25 or luftfeuchtigkeit >= 60:
@@ -78,33 +113,29 @@ while True:
             display.text(font, "Temperatur:", 10, 20)
             display.text(font, "Luftfeuchtigkeit:", 10, 90)
             
-    if power_an:  # Hauptschleife wird nur ausgeführt, wenn power_an True ist
+    if power_an == True:  # Funktionen werden nur ausgeführt, wenn power_an True ist
+        
         # Temperatur und Luftfeuchtigkeit anzeigen
         display.text(font, "%.2f °C" % temperatur, 10, 50)
         display.text(font, "%.2f %%" % luftfeuchtigkeit, 10, 120)
-        if temperatur >= 25 or luftfeuchtigkeit >= 60:
+        
+        if temperatur >= 25 or luftfeuchtigkeit >= 60: # rote LED einschalten, gruene LED ausschalten wenn temp >= 25 Grad Celsius und luftf. >= 60 %
             pin_gruen_led.off()
             pin_rot_led.on()
             utime.sleep(0.1)
             pin_rot_led.off()
-        elif temperatur < 25 or luftfeuchtigkeit < 60:
+        elif temperatur < 25 or luftfeuchtigkeit < 60: # gruene LED einschalten, rote LED ausschalten wenn temp < 25 Grad Celsius und luftf. < 60 %
             pin_rot_led.off()
             pin_gruen_led.on()
-    elif power_an == False:
+            
+    elif power_an == False: # Wenn die Steuerung über den Taster ausgeschaltet wurde, dann ...
+        
         # Display löschen
         display.fill(st7789.BLACK)
+        
         # leds ausschalten
         pin_rot_led.off()
         pin_gruen_led.off()
-    letzter_taster_status = aktueller_taster_status
+        
+    letzter_taster_status = aktueller_taster_status # der letzte Taster Status wird auf den aktueller Taster Zustand gesetzt
     utime.sleep(0.1) # kurze Pause um die CPU zu entlasten
-
-
-
-
-
-# while True:
-#     
-#         power_an.value(led_state) # der led Zustand wird auf den gesetzen Zustand von led_state gesetzt
-#     letzter_taster_status = aktueller_taster_status # der letzte Taster Status wird auf den aktueller Taster Zustand gesetzt
-#     sleep(0.1) # kurze Pause um die CPU zu entlasten 
